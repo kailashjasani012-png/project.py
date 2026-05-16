9f8e4b044a0b2dfff42ae2819ef9058f704ceea0
kailashjasani012-png <kailashjasani012@gmail.com>
2026-05-16 23:10:57 +0530

    Add files via upload

---
+++ pr1.py
@@ -0,0 +1,16 @@
+print("welcome to the interactive personal data collector !n")
+
+name=input("please enter your name:")
+age=int(input("please enter your age:"))
+height=float(input("please enter your height in meters:"))
+fav_number=int(input("please enter your favourite number:"))
+
+print("your birth year is approximately:2009 based on your age of 17")
+
+print(f"name: {name} (type:{type(name)}, memory address:{id(name)})")
+print(f"age: {age} (type:{type(age)}, memory address:{id(age)})")
+print(f"height: {height} {type(height)},memory addeess:{id(height)})")
+print(f"favourite number:{fav_number}{type(fav_number)},memory address:{id(fav_number)})")
+
+print("Thank you for using the personal data collector. Goodbye!")
+
